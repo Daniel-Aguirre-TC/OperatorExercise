d0ea14615339a674fc9f4981edab27cacf009acc
Daniel Aguirre <uhyeay24@gmail.com>
2021-08-20 11:36:44 -0500

    First Commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {a/b} {nameof(remainder)} {remainder}");
+            Console.WriteLine("Now we will get the area of a circle.");
+            AskForRadius();            
+        }
+
+        public static void AreaOfCircle(double radius)
+        {
+            Console.WriteLine(radius * radius * Math.PI);
+        }
+
+        public static void AskForRadius()
+        {
+            Console.WriteLine("Please enter a radius. Non-numerical values are not accepted.");
+            if (Double.TryParse(Console.ReadLine(), out double input))
+                AreaOfCircle(input);
+            else
+            {
+                AskForRadius();
+            }
+        }
+    }
+}
